fc3eb5f2a5d88fa7e0784671bf09eb2cac58294a
derekdlv <derekmorsan@gmail.com>
2025-05-19 02:59:28 -0600

    Disenno de login

--- Main.java
+++ Main.java
@@ -1,36 +1,30 @@
-// Importamos las clases necesarias de la librería Swing, que nos permiten crear interfaces gráficas de usuario
-import javax.swing.JFrame;  // JFrame es la clase que se utiliza para crear una ventana principal.
-import javax.swing.JLabel;  // JLabel es la clase que se usa para mostrar texto o imágenes en la ventana.
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.*;
+
+import vistas.loginPanel;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // Crear una ventana usando JFrame
-        // Aquí estamos creando un objeto de tipo JFrame, que representa la ventana en la interfaz gráfica.
+
         JFrame miVentana = new JFrame(); 
         
-        // Establecemos el título de la ventana con el método setTitle()
-        // En este caso, el título de la ventana será "Hospital SCL".
         miVentana.setTitle("Hospital SCL"); 
-        
-        // Definimos el tamaño de la ventana con el método setSize(), en donde el primer número es el ancho
-        // y el segundo número es la altura de la ventana en píxeles.
         miVentana.setSize(1000, 800); 
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miVentana.setLayout(new BorderLayout());
 
-        // Crear un texto utilizando JLabel
-        // JLabel es un componente que nos permite mostrar texto en la ventana.
-        JLabel texto = new JLabel(); // Aquí creamos un objeto de tipo JLabel vacío.
+        JPanel panelBanner = new JPanel();
+        panelBanner.setPreferredSize(new Dimension(500,800));
+        panelBanner.setOpaque(true);
+        panelBanner.setBackground(Color.RED);
+        miVentana.add(panelBanner, BorderLayout.WEST);
 
-        // Establecemos el texto que queremos mostrar en el JLabel.
-        // En este caso, el texto será "Hola Mundo".
-        texto.setText("Hola Mundo"); 
+        loginPanel loginPanel = new loginPanel();
+        miVentana.add(loginPanel.getPanel(), BorderLayout.CENTER);
 
-        // Agregamos el JLabel (que contiene el texto) a la ventana para que se muestre.
-        miVentana.add(texto); 
-
-        // Finalmente, hacemos que la ventana sea visible. Por defecto, las ventanas en Swing están ocultas,
-        // por lo que debemos llamar a setVisible(true) para que se vea en pantalla.
         miVentana.setVisible(true);
     }
 }
---
+++ vistas/loginPanel.java
@@ -0,0 +1,71 @@
+package vistas;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+
+
+public class loginPanel {
+    private JPanel loginPanel;
+
+    public loginPanel() {
+        loginPanel = new JPanel();
+        loginPanel.setSize(500, 800);
+        loginPanel.setOpaque(true);
+        loginPanel.setLayout(new GridBagLayout());
+
+        //ADMINISTRADOR DE DISENNO
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 80, 10, 80);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 1.0;
+
+        //TITULO
+        JLabel titulo = new JLabel("Login");
+        titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        titulo.setVerticalAlignment(SwingConstants.CENTER);
+        gbc.gridx = 2;
+        gbc.gridy =0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        loginPanel.add(titulo, gbc);
+
+        // Entrada correo
+        JTextField entradaCorreo = new JTextField();
+        entradaCorreo.setPreferredSize(new Dimension(100, 50));
+        loginPanel.add(entradaCorreo);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;  
+        gbc.weightx = 1.0;
+        entradaCorreo.setText("Usuario");
+        loginPanel.add(entradaCorreo, gbc);
+
+        // Entrada contrasenna
+        JTextField entradaContrasenna = new JTextField();
+        entradaContrasenna.setPreferredSize(new Dimension(100, 50));
+        loginPanel.add(entradaContrasenna);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        entradaContrasenna.setText("Contrasenna");
+        loginPanel.add(entradaContrasenna, gbc);
+
+        // Boton 
+        JButton boton = new JButton("Ingresar");
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.fill = GridBagConstraints.NONE;
+        loginPanel.add(boton, gbc);
+    }
+
+    public JPanel getPanel() {
+        return loginPanel;
+    }
+}
+
